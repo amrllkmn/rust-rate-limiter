1a54181c7ca56be356c515405a3bc20f9934f582
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2023-10-09 22:32:14 +0800

    Modify user struct

--- src/users/mod.rs
+++ src/users/mod.rs
@@ -1,8 +1,26 @@
-struct User<'a> {
+use std::thread;
+use std::time::Duration;
+struct User {
     id: u32,
-    bucket: &'a bucket::Bucket,
+    bucket: u32,
 }
 
-mod bucket {
-    pub struct Bucket {}
+impl User {
+    pub fn new(id: u32) -> User {
+        User { id, bucket: 0 }
+    }
+
+    pub fn start(&mut self) {
+        loop {
+            if self.bucket < 10 {
+                // Sleep for one second
+                thread::sleep(Duration::from_secs(1));
+                self.bucket += 1
+            }
+        }
+    }
+
+    pub fn consume(&mut self) {
+        self.bucket -= 1
+    }
 }
