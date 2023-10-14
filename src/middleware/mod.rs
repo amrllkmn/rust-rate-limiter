f722e5f19ea23f648cab0b1562663ebe22ce190c
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2023-10-14 22:28:01 +0800

    Modify middleware for multi-threaded processing

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -1,14 +1,20 @@
 use crate::{users::User, AppState};
-use axum::{extract::State, http::Request, middleware::Next, response::Response};
+use axum::{
+    extract::State,
+    http::{Request, StatusCode},
+    middleware::Next,
+    response::IntoResponse,
+};
 
 pub async fn token_bucket<B>(
     State(state): State<AppState>,
     req: Request<B>,
     next: Next<B>,
-) -> Response {
+) -> Result<impl IntoResponse, StatusCode> {
+    println!("The middleware came first");
     let header = req.headers();
-    // I know this is dirty, but we can
-    let userId = header
+    // I know this is dirty, but we can fix it later
+    let user_id = header
         .get("userid")
         .unwrap()
         .to_str()
@@ -16,7 +22,25 @@ pub async fn token_bucket<B>(
         .parse::<u32>()
         .unwrap();
 
-    let user: User = User::new(userId);
-    println!("The middleware came first");
-    next.run(req).await
+    let mut users = state.lock().await;
+
+    println!("{:?}", users);
+
+    if let Some(user) = users.iter_mut().find(|user| user.id == user_id) {
+        if user.bucket_is_empty() {
+            return Err(StatusCode::TOO_MANY_REQUESTS);
+        }
+        user.consume();
+
+        println!("The user: {:?}", user);
+    } else {
+        let mut user: User = User::new(user_id);
+
+        user.start();
+
+        users.push(user);
+    }
+
+    let resp = next.run(req).await;
+    Ok(resp)
 }
