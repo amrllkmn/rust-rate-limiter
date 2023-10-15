23671db9ba23d908560f7452cc608b5d51be2d7d
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2023-10-15 15:08:40 +0800

    Modify functions to become async

--- src/users/mod.rs
+++ src/users/mod.rs
@@ -1,34 +1,44 @@
-use std::thread;
+use std::sync::Arc;
 use std::time::Duration;
+use tokio::sync::Mutex;
 
 #[derive(Clone, Debug)]
 pub struct User {
     pub id: u32,
-    bucket: u32,
+    bucket: Arc<Mutex<u32>>,
 }
 
 impl User {
-    pub fn new(id: u32) -> User {
-        User { id, bucket: 0 }
+    pub async fn new(id: u32) -> User {
+        let user = User {
+            id,
+            bucket: Arc::new(Mutex::new(0)),
+        };
+        // Clone user for the task.
+        let user_clone = user.clone();
+        tokio::spawn(async move {
+            user_clone.start().await;
+        });
+        user
     }
 
-    pub fn start(&mut self) {
+    pub async fn start(&self) {
         loop {
-            if self.bucket < 10 {
-                // Sleep for one second
-                thread::sleep(Duration::from_secs(1));
-                self.bucket += 1
-            } else {
-                break;
+            tokio::time::sleep(Duration::from_secs(1)).await;
+            let mut bucket = self.bucket.lock().await;
+            if *bucket < 10 {
+                *bucket += 1
             }
         }
     }
 
-    pub fn consume(&mut self) {
-        self.bucket -= 1
+    pub async fn consume(&self) {
+        let mut bucket = self.bucket.lock().await;
+        *bucket -= 1;
     }
 
-    pub fn bucket_is_empty(&self) -> bool {
-        self.bucket < 1
+    pub async fn bucket_is_empty(&self) -> bool {
+        let bucket = self.bucket.lock().await;
+        *bucket < 1
     }
 }
